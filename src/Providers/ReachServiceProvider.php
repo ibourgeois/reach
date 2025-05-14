25354812088dc7621b787b2dd59c671e4a079a60
Derek Bourgeois <derek@devoption.io>
2025-05-14 01:55:45 -0400

    feature: add service provider for laravel

---
+++ src/Providers/ReachServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace iBourgeois\Reach\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReachServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
