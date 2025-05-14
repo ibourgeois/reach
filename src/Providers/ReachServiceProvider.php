72769627cf7a5032d64d7b4026809f4e9ddde33e
Derek Bourgeois <derek@devoption.io>
2025-05-14 02:33:20 -0400

    feature: add database migration for lists

--- src/Providers/ReachServiceProvider.php
+++ src/Providers/ReachServiceProvider.php
@@ -23,6 +23,23 @@ class ReachServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->publishMigrations();
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    /** 
+     * Publish the database migrations.
+     * 
+     * @return void
+     */
+    public function publishMigrations(): void
+    {
+        if ($this->app->runningInConsole()) {
+            if (! class_exists('CreateListsTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/2025_05_14_062102_create_lists_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_lists_table.php'),
+                ], 'migrations');
+            }
+        }
     }
 }
